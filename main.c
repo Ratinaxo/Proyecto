070fbbd6ddd3d09add25adebf8aadf00715810fe
Ricardo <ricardo.toro.m@mail.pucv.cl>
2023-06-07 02:59:33 -0400

    Detalles main y menus

--- main.c
+++ main.c
@@ -2,38 +2,52 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 #include "list.h"
 #include "map.h"
-
-#define barrita "|"
+#include "colors.h"
 
 typedef unsigned short uShort;
 
 typedef struct{
   int numero;
   char *terreno;
-
+  List *hexagonosAdyacentes;
 }tipoHexagono;
+
+typedef struct{
+  char *nombre;
+  uShort puntos;
+  List *recursos;
+  List *edificios;
+}tipoJugador;
+
+typedef struct{
+  tipoHexagono *hexagono;
+}tipoTablero;
+
+tipoTablero cerarNuevoTablero(){
+
+  return;
+}
 
 void crearNuevaPartida(){
 
   return;
 }
 
-uShort dado(){
+uShort unDado(){
   return (rand() % 6) + 1;
 }
 
 uShort dosDados(){
-  return dado() + dado();
+  return unDado() + unDado();
 }
 
 void instrucciones(){
   int opcionInstrucciones;
-  
-
   while (true){
-    printf("1. Conocimientos generales\n");
+    printf("\n1. Conocimientos generales\n");
     printf("2. Turnos\n");
     printf("0. Volver al menu principal\n");
     scanf("%d", &opcionInstrucciones);
@@ -42,14 +56,14 @@ void instrucciones(){
       case 0:
         return;
       case 1:
-        printf(" ___________________________________________________________________________________________________________________________________________\n");
+        printf("+-------------------------------------------------------------------------------------------------------------------------------------------+\n");
         printf("|Catan es un juego de estrategia que consiste en conseguir 10 puntos a traves de la construccion de edificios y la recoleccion de materiales|\n");
-        printf("|Los materiales se consiguen a traves de los hexagonos que conforman el tablero %61s\n", barrita);
-        printf("|Cada hexagono tiene un numero y un terreno %97s\n", barrita);
-        printf("|Los numeros del hexagono seran dados a traves de un dado %83s\n", barrita);
-        printf("|Los terrenos son: bosques (maderas), colinas (ladrillos), praderas (ovejas), cultivos (trigos) y canteras (rocas) %26s\n", barrita);
-        printf("|Los edificios que se pueden construir son: caminos, pueblos y ciudades %69s\n", barrita);
-        printf("|Los caminos se construyen con 1 ladrillo y 1 madera %88s\n", barrita);
+        printf("|Los materiales se consiguen a traves de los hexagonos que conforman el tablero                                                             |\n");
+        printf("|Cada hexagono tiene un numero y un terreno                                                                                                 |\n");
+        printf("|Los numeros del hexagono seran dados a traves de un dado                                                                                   |\n");
+        printf("|Los terrenos son: bosques (maderas), colinas (ladrillos), praderas (ovejas), cultivos (trigos) y canteras (rocas)                          |\n");
+        printf("|Los edificios que se pueden construir son: caminos, pueblos y ciudades                                                                     |\n");
+        printf("|Los caminos se construyen con 1 ladrillo y 1 madera                                                                                        |\n");
         printf("|Los pueblos se construyen con 1 ladrillo, 1 madera, 1 oveja y 1 trigo                                                                      |\n");
         printf("|Las ciudades se construyen con 2 trigos y 3 rocas                                                                                          |\n");
         printf("|Los pueblos generan 1 punto y las ciudades 2 puntos                                                                                        |\n");
@@ -59,21 +73,22 @@ void instrucciones(){
         printf("|Los pueblos y ciudades deben estar conectados a un camino                                                                                  |\n");
         printf("|Los pueblos deben estar a 2 caminos de distancia entre si                                                                                  |\n");
         printf("|Los pueblos y ciudades no pueden estar a 1 camino de distancia entre si                                                                    |\n");
-        printf(" ___________________________________________________________________________________________________________________________________________\n");
+        printf("+-------------------------------------------------------------------------------------------------------------------------------------------+\n");
         break;
 
       case 2:
-        printf("Para comenzar la partida:\n");
-        printf("1. Lanzar el dado para decidir el orden de los jugadores\n");
-        printf("2. El orden para colocar los primeros edificios es el siguiente: jugador 1, jugador 2, jugador 3 y jugador 4\n");
-        printf("Y luego: jugador 4, jugador 3, jugador 2 y jugador 1\n");
-        printf("3. Cada jugador debe de haber colocado 2 pueblos y 2 caminos en total en la posicion que desee\n");
-        printf("4. Luego de que todos los jugadores hayan colocado sus edificios, se procede a lanzar el dado\n");
-        printf("5. El ultimo jugador que haya construido su edificio sera el primero en lanzar el dado\n");
-        printf("6. Todos los jugadores obtendran los materiales correspondientes dependiendo del numero que haya salido en el dado\n");
 
+        printf("+-------------------------------------------------------------------------------------------------------------------------------------------+\n");
+        printf("|Para comenzar la partida:                                                                                                                  |\n");
+        printf("|1. Lanzar el dado para decidir el orden de los jugadores                                                                                   |\n");
+        printf("|2. El orden para colocar los primeros edificios es el siguiente: jugador 1, jugador 2, jugador 3 y jugador 4                               |\n");
+        printf("|Y luego: jugador 4, jugador 3, jugador 2 y jugador 1                                                                                       |\n");
+        printf("|3. Cada jugador debe de haber colocado 2 pueblos y 2 caminos en total en la posicion que desee                                             |\n");
+        printf("|4. Luego de que todos los jugadores hayan colocado sus edificios, se procede a lanzar el dado                                              |\n");
+        printf("|5. El ultimo jugador que haya construido su edificio sera el primero en lanzar el dado                                                     |\n");
+        printf("|6. Todos los jugadores obtendran los materiales correspondientes dependiendo del numero que haya salido en el dado                         |\n");
+        printf("+-------------------------------------------------------------------------------------------------------------------------------------------+\n");
 
-    
         break;
 
       default:
@@ -98,24 +113,28 @@ void generarNuevoTablero(){
 }
 
 int main(){
+  printf("UTILIZAR ESTE PROGRAMA EN PANTALLA COMPLETA!!\n\n");
   srand(time(NULL));
   int opcionMenuPrincipal;
-  printf("1. Crear nueva partida\n");
-  printf("2. Instrucciones\n");
-  do{
-    printf("Ingrese una opcion: ");
-    scanf("%d", &opcionMenuPrincipal);
-
-    if (opcionMenuPrincipal < 0 || opcionMenuPrincipal > 2)
-      printf("Ingrese una opcion valida\n");
-
-  }while (opcionMenuPrincipal < 0 || opcionMenuPrincipal > 2);
 
   while (true){
+    do{
+      printf(" ------------------------\n");
+      printf("|1. Crear nueva partida  |\n");
+      printf("|2. Instrucciones        |\n");
+      printf("|0. Salir                |\n");
+      printf(" ------------------------\n");
+      printf("Ingrese una opcion: ");
+      scanf("%d", &opcionMenuPrincipal);
+      if (opcionMenuPrincipal < 0 || opcionMenuPrincipal > 2)
+        printf("Ingrese una opcion valida\n");
+      printf("\n__________________________________________________________________________________________\n");
+    }while (opcionMenuPrincipal < 0 || opcionMenuPrincipal > 2);
+
     switch (opcionMenuPrincipal)
     {
     case 0:
-      printf("Gracias por jugar\n");
+      printf("\nGracias por jugar\n");
       exit(EXIT_SUCCESS);
     case 1:
       crearNuevaPartida();
---
+++ test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+//hacer un hexágono
+
+int main(){
+    printf("           / \\     / \\     / \\\n");
+    printf("          /   \\   /   \\   /   \\\n");
+    printf("         /     \\ /     \\ /     \\\n");
+    printf("        |  A1   |       |       |\n");
+    printf("        |       |       |       |\n");
+    printf("       / \\     / \\     / \\     / \\\n");
+    printf("      /   \\   /   \\   /   \\   /   \\\n");
+    printf("     /     \\ /     \\ /     \\ /     \\\n");
+    printf("    |       |       |       |       |\n");
+    printf("    |       |       |       |       | \n");
+    printf("   / \\     / \\     / \\     / \\     / \\\n");
+    printf("  /   \\   /   \\   /   \\   /   \\   /   \\\n");
+    printf(" /     \\ /     \\ /     \\ /     \\ /     \\\n");
+    printf("|       |       |       |       |       |\n");
+    printf("|       |       |       |       |       |\n");
+    printf(" \\     / \\     / \\     / \\     / \\     /\n");
+    printf("  \\   /   \\   /   \\   /   \\   /   \\   /\n");
+    printf("   \\ /     \\ /     \\ /     \\ /     \\ /\n");
+    printf("    |       |       |       |       |\n");
+    printf("    |       |       |       |       |\n");
+    printf("     \\     / \\     / \\     / \\     /\n");
+    printf("      \\   /   \\   /   \\   /   \\   /\n");
+    printf("       \\ /     \\ /     \\ /     \\ /\n");
+    printf("        |   A2  |       |       |\n");
+    printf("        |       |       |       |\n");
+    printf("         \\     / \\     / \\     /\n");
+    printf("          \\   /   \\   /   \\   /\n");
+    printf("           \\ /     \\ /     \\ /\n");
+}
